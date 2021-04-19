e02b10d64575f90e8bafc26f190080731e3b19e3
Kamil <kamil.krol@gmail.com>
2021-04-19 20:28:30 +0200

    zakomentowanie file readera

--- kodilla-exception/src/test/java/com/kodilla/exception/io/FileReaderTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/FileReaderTestSuite.java
@@ -1,15 +1,17 @@
 package com.kodilla.exception.io;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
-public class FileReaderTestSuite {
-    @Test
-    void testReadFile() {
-        // given
-        FileReader fileReader = new FileReader();
-        // when & then
-        assertDoesNotThrow(() -> fileReader.readFile());
-    }
-}
+//import jdk.jfr.Enabled;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+//
+//public class FileReaderTestSuite {
+//    @Enabled
+//    @Test
+//    void testReadFile() {
+//        // given
+//        FileReader fileReader = new FileReader();
+//        // when & then
+//        assertDoesNotThrow(() -> fileReader.readFile());
+//    }
+//}
