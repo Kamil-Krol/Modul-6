179fee764cf27ede620e11fd738ad038a525b315
Kamil <kamil.krol@gmail.com>
2021-09-22 15:09:33 +0200

    Zadanie 14.4

---
+++ kodilla-spring-web-nowy/src/main/java/com/kodilla/spring/web/nowy/KodillaSpringWebNowyApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.web.nowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpringWebNowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSpringWebNowyApplication.class, args);
+    }
+
+}
